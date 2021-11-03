50a0ae8df04fa964dcc500e54838e39dcb3b8514
Ahmad Salman Malik <malik.salman96@gmail.com>
2021-11-03 16:27:58 +0530

    commit

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb'
+dotenv.config();
+const app = express();
+app.use(express.json());
+const port = 5000;
+
+
+const MONGO_DB1 = process.env.MONGO_DB;
+async function createDBConnection(){
+    const client = new MongoClient(MONGO_DB1);
+    await client.connect();
+    console.log("Connected to Atlas DB");
+    return client;
+}
+
+app.get("/", (request, response) => {
+    response.send("Welcome to Room Booking API")
+})
+
+app.post("/create-room", async (request, response) => {
+    const roomData = request.body;
+    console.log(roomData);
+    const client = await createDBConnection();
+    const result = await client 
+    .db("hall-booking")
+    .collection("room")
+    .insertMany(request.body);
+
+    response.send(result)
+});
+app.post("/book-room", async(request, response) => {
+    const  userData = request.body;
+    console.log(userData);
+    const client = await createDBConnection();
+    const result = await client.db("hall-booking")
+    .collection("room")
+    .insertOne(request.body);
+    response.send(result);
+});
+
+app.get("/list-all-booked-room", async(request, response) => {
+
+});
+
+app.get("/list-all-customers-data", async(request, response) => {
+
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log("The server is started at: ", port))
